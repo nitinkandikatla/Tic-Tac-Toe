1009c152f1a297cce4e14a25bc7dc35382038e7e
nitinkandikatla <nitinkandikatla99@gmail.com>
2021-01-14 14:38:13 +0530

    ADDED : printing board

--- TicTocToe.java
+++ TicTocToe.java
@@ -18,7 +18,7 @@ public class TicTocToe {
 			computer='X';
 		}
 		System.out.println("Player :" +player+ " computer :" + computer);
-	//	printBoard(board);
+		printBoard(board);
 	}
 	private static char[] createBoard() {
 		char[] board=new char[10];
@@ -36,7 +36,14 @@ public class TicTocToe {
 		return player;
 		
 	}
-	
+	public static void printBoard(char board[]) {
+		 System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |"); 
+		 System.out.println("|-----------|");
+		 System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
+		 System.out.println("|-----------|");
+		 System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
+		 System.out.println("|-----------|");
+	}
 	
 
 }
