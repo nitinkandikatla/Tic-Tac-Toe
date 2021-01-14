d4c653394df95b435e9fe197ae4a18b8ae06a11f
nitinkandikatla <nitinkandikatla99@gmail.com>
2021-01-14 13:45:25 +0530

    ADDED : created board

--- TicTocToe.java
+++ TicTocToe.java
@@ -4,8 +4,15 @@ public class TicTocToe {
 
 	public static void main(String[] args) {
 		System.out.println(" Welcome ");
-		
-
+		char[] board=createBoard();
+	}
+	private static char[] createBoard() {
+		char[] board=new char[10];
+		for(int i=1	;i<board.length;i++)
+		{
+			board[i]=' ';
+		}
+		return board;
 	}
 
 }
